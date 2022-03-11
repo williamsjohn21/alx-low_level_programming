c52342d164d3e62230d7ebeb8970b8e3756acdd2
williamsjohn21 <dejuwon11@gmail.com>
2022-03-10 19:38:10 -0800

    my tesk 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - to print Programming is like building a multilingual puzzle
+ * Return: should print 0 
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
