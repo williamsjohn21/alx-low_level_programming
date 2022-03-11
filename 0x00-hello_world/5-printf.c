6800262e23eccd65314f093a69829ee92f6b15fd
williamsjohn21 <dejuwon11@gmail.com>
2022-03-10 20:09:11 -0800

    my tesk 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - block
+ * Return: return 1 after exexution
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
