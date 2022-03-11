9f1ea2e6375163b341041e1ebcbc1339419cdcf7
williamsjohn21 <dejuwon11@gmail.com>
2022-03-10 21:29:21 -0800

    my tesk 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - Access point
+ * 
  * Return: 1
  */
 int main(void)
 {
-char ch[] = fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-write(2,  ch, sizeof(ch));
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
