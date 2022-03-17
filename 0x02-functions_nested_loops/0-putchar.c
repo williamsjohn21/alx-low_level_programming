801f58204d909509456034754503a076e7c439f2
williamsjohn21 <dejuwon11@gmail.com>
2022-03-17 15:13:07 -0700

    My 0-putchar task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
 #include "main.h"
 
-
 /**
  * main - Entry point
- *
- * Return: nothing, return void
+ * Return:0
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
 _putchar('\n');
-
 return (0);
 }
